23721f60905788fde2711bb8d1cf22b5dca0bf3d
Parvink <thomas.kauffmann@epitech.eu>
2022-05-02 18:56:42 +0200

    Database link setup from Matthieu atlas

--- db/config.js
+++ db/config.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose'
 import Logger from '../utils/logger.js';
 
 const connect = async => {
-    const dbUri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@clusterdashboard.lkbnt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-  
+    const dbUri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@vandoeuvreechecsdb.kvkll.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     return mongoose
       .connect(dbUri)
       .then(() => {
+        Logger.info("On est dans la db")
       })
       .catch((error) => {
         Logger.error(error)
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import * as dotenv from 'dotenv';
 
 import connect from './db/config.js'
+import loggerMiddleware from './middleware/logger.middleware.js'
 
 dotenv.config()
 const port = process.env.PORT || 3000
@@ -10,7 +11,14 @@ const app = express()
 app.use(express.json())
 
 connect();
-app.use('/', (req, res, next) => {
+app.use('user/:id', (req, res, next) => {
+    res.send(req.params.id);
 })
 
 app.listen(port)
+
+/*getUsers() //get all users de la db
+getUserById() // get an user by his id
+updateUserById() //
+deleteUserById()
+createUser()*/
