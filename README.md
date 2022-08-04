062675c30aef5e680764ed85b7621bce1e1e5347
Parvink <thomas.kauffmann@epitech.eu>
2022-08-04 12:17:16 +0200

    Basic info on README and new packages

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # VandoeuvreEchecsAPI
+
+#### Welcome to the repository of the VandoeuvreEchecs server
+
+## Docker
+#### To build the server on Docker, you must have Docker installed and use the following command:
+
+
+```docker build -t server .```
+#
+and to run it:
+#
+```docker run -p 3000:3000 server```
+#
+
+## Npm
+#
+
+#### To build the server without Docker, you need to have **npm** and **node** installed on your shell.
+#
+
+To install the necessary package, you need to start with the command:
+
+```npm install```
+#
+and then you should be able to run it with:
+#
+```npm start```
--- db/config.js
+++ db/config.js
@@ -1,17 +1,15 @@
 import mongoose from 'mongoose'
-import Logger from '../utils/logger.js';
+import Logger from '../utils/logger.js'
 
-const connect = async => {
+const connect = () => {
     const dbUri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@vandoeuvreechecsdb.kvkll.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     return mongoose
-      .connect(dbUri)
-      .then(() => {
-        console.log("On est dans la db")
-      })
-      .catch((error) => {
-        Logger.error(error)
-        process.exit(1);
-      });
-  };
+        .connect(dbUri)
+        .then(() => {})
+        .catch((error) => {
+            Logger.error(error)
+            process.exit(1)
+        })
+}
 
 export default connect
